7218bc37e364f4af1dd98ada27418bd648697184
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-05-23 14:24:56 -0400

    Added highlighting functionality following the mouse
    + cleaned code up

--- pixel.js
+++ pixel.js
@@ -20,30 +20,61 @@ export default class PixelPlugin
 
     // Subscribes to VisibleTilesDidLoad event to start drawing highlights.
     // Takes an array of highlighted objects
-    activatePlugin(layers)
+    activatePlugin()
     {
-        let handle = this.subscribeToEvent(layers);
+        if(this.layers === null){
+            // Create the array of highlights to pass to drawHighlights function
+            let highlight1 = new HighlightArea(23, 42, 24, 24, 0);
+            // highlight2 = new HighlightArea(48, 50, 57, 5, 0),
+            // highlight3 = new HighlightArea(75, 80, 30, 10, 0),
+            // highlight4 = new HighlightArea(21, 77, 12, 13.5, 0),
+            // highlight5 = new HighlightArea(50, 120, 50, 10, 0),
+            // highlight6 = new HighlightArea(30, 180, 60, 20, 0);
+            let highlighted1 = [highlight1];
+            // highlighted2 = [highlight3, highlight4],
+            // highlighted3 = [highlight5, highlight6];
+            let layer1 = new Layer(0,0.3, highlighted1);
+            // layer2 = new Layer(1,0.5, highlighted2),
+            // layer3 = new Layer(2,0.3, highlighted3);
+            this.layers = [layer1];
+        }
+
+        let handle = this.subscribeToEvent();
         this.core.getSettings().renderer._paint();  // Repaint the tiles to retrigger VisibleTilesDidLoad
         this.activated = true;
-        layers.forEach((layer) => {
+        this.layers.forEach((layer) => {
             this.createPluginElements(layer);
         });
+
+        var canvas = document.getElementById("diva-1-outer");
+        canvas.addEventListener('mousemove', (evt) =>
+        {
+            let zoomLevel = this.core.getSettings().zoomLevel;
+            let scaleRatio = Math.pow(2,zoomLevel);
+
+            var mousePos = this.getMousePos(canvas, evt);
+            var relativeCoords = this.getRelativeCoordinates(mousePos.x, mousePos.y);
+            let highlight = new HighlightArea(relativeCoords.x, relativeCoords.y, 1/10, 1/10, 0);
+            this.layers[0].addHighlightToLayer(highlight);
+            this.core.getSettings().renderer._paint();
+        }, false);
+
         return handle;
     }
 
-    deactivatePlugin(layers)
+    deactivatePlugin()
     {
         Diva.Events.unsubscribe(this.handle);
         this.core.getSettings().renderer._paint(); // Repaint the tiles to make the highlights disappear off the page
         this.activated = false;
-        layers.forEach((layer) => {
+        this.layers.forEach((layer) => {
             this.destroyPluginElements(layer);
         });
     }
 
     createPluginElements(layer)
     {
-        var x = document.createElement("INPUT");
+        var x = document.createElement("input");
         x.setAttribute("id", "layer " + layer.layerType);
         x.setAttribute("type", "range");
         x.setAttribute('max', 100);
@@ -65,20 +96,52 @@ export default class PixelPlugin
         document.body.removeChild(rangeInput);
     }
 
-    subscribeToEvent(layers){
+    subscribeToEvent(){
         let handle = Diva.Events.subscribe('VisibleTilesDidLoad', (args) =>
         {
-            this.drawHighlights(layers, args);
+            this.drawHighlights(args);
         });
         return handle;
     }
 
-    drawHighlights(layers, args)
+    getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+
+    getRelativeCoordinates(highlightXOffset, highlightYOffset)
+    {
+        let pageIndex = this.core.getSettings().currentPageIndex;
+        let zoomLevel = this.core.getSettings().zoomLevel;
+
+        let renderer = this.core.getSettings().renderer;
+        let scaleRatio = Math.pow(2,zoomLevel);
+
+        const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
+        const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
+
+        // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
+        // (to make it look like it is on top of a page in Diva)
+        let absoluteRectOriginX = highlightXOffset - renderer._getImageOffset(pageIndex).left + renderer._viewport.left -  viewportPaddingX;
+        let absoluteRectOriginY = highlightYOffset - renderer._getImageOffset(pageIndex).top + renderer._viewport.top - viewportPaddingY;
+
+        return {
+            x: absoluteRectOriginX/scaleRatio,
+            y: absoluteRectOriginY/scaleRatio
+        };
+
+
+    }
+
+    drawHighlights(args)
     {
         var pageIndex = args[0],
             zoomLevel = args[1];
 
-        layers.forEach((layer) => {
+        this.layers.forEach((layer) => {
             var highlights = layer.highlights;
 
             highlights.forEach((highlighted) =>
@@ -140,32 +203,16 @@ export default class PixelPlugin
 
     /**
      * Enables the layering plugin and stops it from being repetitively called.
-     *
      **/
     handleClick (event, settings, publicInstance, pageIndex)
     {
-        // Create the array of highlights to pass to drawHighlights function
-        let highlight1 = new HighlightArea(23, 42, 24, 24, 0),
-            highlight2 = new HighlightArea(48, 50, 57, 5, 0),
-            highlight3 = new HighlightArea(75, 80, 30, 10, 0),
-            highlight4 = new HighlightArea(21, 77, 12, 13.5, 0),
-            highlight5 = new HighlightArea(50, 120, 50, 10, 0),
-            highlight6 = new HighlightArea(30, 180, 60, 20, 0);
-        let highlighted1 = [highlight1, highlight2],
-            highlighted2 = [highlight3, highlight4],
-            highlighted3 = [highlight5, highlight6];
-        let layer1 = new Layer(0,0.3, highlighted1),
-            layer2 = new Layer(1,0.5, highlighted2),
-            layer3 = new Layer(2,0.3, highlighted3);
-        let layers = [layer1, layer2, layer3];
-
         if (!this.activated)
         {
-            this.handle = this.activatePlugin(layers);
+            this.handle = this.activatePlugin();
         }
         else
         {
-            this.deactivatePlugin(layers);
+            this.deactivatePlugin();
         }
     }
 
