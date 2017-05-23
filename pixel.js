8e5a2fe7205c74bdf8abec665225ea97636ca277
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-05-23 16:52:51 -0400

    Added support for drawing paths by holding down the mouse
    Now the user can draw random paths by holding the left mouse click and
    dragging where they want. The code structure also supports the
    implementation of undo/redo actions.

--- pixel.js
+++ pixel.js
@@ -17,12 +17,16 @@ export default class PixelPlugin
         this.handle = null;
         this.layers = null;
         this.matrix = null;
+        this.mousePressed = false;
+        this.lastX, this.lastY;
     }
 
     // Subscribes to VisibleTilesDidLoad event to start drawing highlights.
     // Takes an array of highlighted objects
     activatePlugin()
     {
+        this.core.disableScrollable();
+        
         if(this.layers === null){
             // Create the array of highlights to pass to drawHighlights function
             let highlight1 = new HighlightArea(23, 42, 24, 24, 0);
@@ -48,20 +52,65 @@ export default class PixelPlugin
         });
 
         var canvas = document.getElementById("diva-1-outer");
-        canvas.addEventListener('mousemove', (evt) =>
+
+        canvas.addEventListener('mousedown', (evt) =>
         {
+            this.mousePressed = true;
+            let pageIndex = this.core.getSettings().currentPageIndex;
             let zoomLevel = this.core.getSettings().zoomLevel;
-            let scaleRatio = Math.pow(2,zoomLevel);
 
             var mousePos = this.getMousePos(canvas, evt);
             var relativeCoords = this.getRelativeCoordinates(mousePos.x, mousePos.y);
-            let highlight = new HighlightArea(relativeCoords.x, relativeCoords.y, 1/10, 1/10, 0);
-            this.layers[0].addHighlightToLayer(highlight);
-            this.core.getSettings().renderer._paint();
+            let point = new Point(relativeCoords.x, relativeCoords.y, pageIndex);
+            this.layers[0].createNewPath(point);
+
+
+            // var mousePos = this.getMousePos(canvas, evt);
+            // var relativeCoords = this.getRelativeCoordinates(mousePos.x, mousePos.y);
+            //let highlight = new HighlightArea(relativeCoords.x, relativeCoords.y, 1, 1, pageIndex);
+            // this.layers[0].addAreaToLayer(highlight);
+            //this.drawHighlight(this.layers[0], highlight, pageIndex, zoomLevel);
+            this.drawPath(this.layers[0], point, pageIndex, zoomLevel, false);
+        });
+
+        canvas.addEventListener('mouseup', (evt) =>
+        {
+            this.mousePressed = false;
+        });
+
+        canvas.addEventListener('mouseleave', (evt) =>
+        {
+            this.mousePressed = false;
+        });
+
+        canvas.addEventListener('mousemove', (evt) =>
+        {
+            if (this.mousePressed)
+            {
+                this.mousePressed = true;
+                let pageIndex = this.core.getSettings().currentPageIndex;
+                let zoomLevel = this.core.getSettings().zoomLevel;
+
+                var mousePos = this.getMousePos(canvas, evt);
+                var relativeCoords = this.getRelativeCoordinates(mousePos.x, mousePos.y);
+                let point = new Point(relativeCoords.x, relativeCoords.y, pageIndex);
+                this.layers[0].addToLastPath(point);
+
+                // let pageIndex = this.core.getSettings().currentPageIndex;
+                // let zoomLevel = this.core.getSettings().zoomLevel;
+                //
+                // var mousePos = this.getMousePos(canvas, evt);
+                // var relativeCoords = this.getRelativeCoordinates(mousePos.x, mousePos.y);
+                // let highlight = new HighlightArea(relativeCoords.x, relativeCoords.y, 1, 1, pageIndex);
+                // this.layers[0].addAreaToLayer(highlight);
+                //this.drawHighlight(this.layers[0], highlight, pageIndex, zoomLevel);
+                this.drawPath(this.layers[0], point, pageIndex, zoomLevel, true);
+            }
         }, false);
 
         return handle;
     }
+
 
     deactivatePlugin()
     {
@@ -133,9 +182,127 @@ export default class PixelPlugin
             x: absoluteRectOriginX/scaleRatio,
             y: absoluteRectOriginY/scaleRatio
         };
-
-
     }
+
+    drawHighlight(layer, highlighted, pageIndex, zoomLevel)
+    {
+        let opacity = layer.opacity;
+        let renderer = this.core.getSettings().renderer;
+        let scaleRatio = Math.pow(2,zoomLevel);
+
+        const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
+        const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
+
+        // The following absolute values are experimental values to highlight the square on the first page of Salzinnes, CDN-Hsmu M2149.L4
+        // The relative values are used to scale the highlights according to the zoom level on the page itself
+        let absoluteRectOriginX = highlighted.relativeRectOriginX * scaleRatio;
+        let absoluteRectOriginY = highlighted.relativeRectOriginY * scaleRatio;
+        let absoluteRectWidth = highlighted.relativeRectWidth * scaleRatio;
+        let absoluteRectHeight = highlighted.relativeRectHeight * scaleRatio;
+
+        // This indicates the page on top of which the highlights are supposed to be drawn
+        let highlightPageIndex = highlighted.pageIndex;
+
+        if (pageIndex === highlightPageIndex)
+        {
+            // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
+            // (to make it look like it is on top of a page in Diva)
+            let highlightXOffset = renderer._getImageOffset(pageIndex).left - renderer._viewport.left + viewportPaddingX + absoluteRectOriginX;
+            let highlightYOffset = renderer._getImageOffset(pageIndex).top - renderer._viewport.top + viewportPaddingY + absoluteRectOriginY;
+
+            //Draw the rectangle
+            let rgba = null;
+            switch (layer.layerType)
+            {
+                case 0:
+                    rgba = "rgba(51, 102, 255, " + opacity + ")";
+                    break;
+                case 1:
+                    rgba = "rgba(255, 51, 102, " + opacity + ")";
+                    break;
+                case 2:
+                    rgba = "rgba(255, 255, 10 , " + opacity + ")";
+                    break;
+                case 3:
+                    rgba = "rgba(10, 255, 10, " + opacity + ")";
+                    break;
+                case 4:
+                    rgba = "rgba(120, 0, 120, " + opacity + ")";
+                    break;
+                default:
+                    rgba = "rgba(255, 0, 0, " + opacity + ")";
+            }
+
+            renderer._ctx.fillStyle = rgba;
+            renderer._ctx.fillRect(highlightXOffset, highlightYOffset,absoluteRectWidth,absoluteRectHeight);
+        }
+    }
+
+    drawPath(layer, point, pageIndex, zoomLevel, isDown)
+    {
+        let opacity = layer.opacity;
+        let renderer = this.core.getSettings().renderer;
+        let scaleRatio = Math.pow(2,zoomLevel);
+
+        const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
+        const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
+
+        // The following absolute values are experimental values to highlight the square on the first page of Salzinnes, CDN-Hsmu M2149.L4
+        // The relative values are used to scale the highlights according to the zoom level on the page itself
+        let absoluteRectOriginX = point.relativeOriginX * scaleRatio;
+        let absoluteRectOriginY = point.relativeOriginY * scaleRatio;
+
+        // This indicates the page on top of which the highlights are supposed to be drawn
+        let highlightPageIndex = point.pageIndex;
+
+        if (pageIndex === highlightPageIndex)
+        {
+            // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
+            // (to make it look like it is on top of a page in Diva)
+            let highlightXOffset = renderer._getImageOffset(pageIndex).left - renderer._viewport.left + viewportPaddingX + absoluteRectOriginX;
+            let highlightYOffset = renderer._getImageOffset(pageIndex).top - renderer._viewport.top + viewportPaddingY + absoluteRectOriginY;
+
+            //Draw the rectangle
+            let rgba = null;
+            switch (layer.layerType)
+            {
+                case 0:
+                    rgba = "rgba(51, 102, 255, " + opacity + ")";
+                    break;
+                case 1:
+                    rgba = "rgba(255, 51, 102, " + opacity + ")";
+                    break;
+                case 2:
+                    rgba = "rgba(255, 255, 10 , " + opacity + ")";
+                    break;
+                case 3:
+                    rgba = "rgba(10, 255, 10, " + opacity + ")";
+                    break;
+                case 4:
+                    rgba = "rgba(120, 0, 120, " + opacity + ")";
+                    break;
+                default:
+                    rgba = "rgba(255, 0, 0, " + opacity + ")";
+            }
+
+            if(isDown)
+            {
+                renderer._ctx.beginPath();
+                renderer._ctx.strokeStyle = rgba;
+                renderer._ctx.lineWidth = 3;
+                renderer._ctx.lineJoin = "round";
+                renderer._ctx.moveTo(this.lastX, this.lastY);
+                renderer._ctx.lineTo(highlightXOffset, highlightYOffset);
+                renderer._ctx.closePath();
+                renderer._ctx.stroke();
+            }
+
+            this.lastX = highlightXOffset;
+            this.lastY = highlightYOffset;
+        }
+    }
+
+
 
     drawHighlights(args)
     {
@@ -143,62 +310,26 @@ export default class PixelPlugin
             zoomLevel = args[1];
 
         this.layers.forEach((layer) => {
-            var highlights = layer.highlights;
+            var highlights = layer.areas;
 
             highlights.forEach((highlighted) =>
                 {
-                    let opacity = layer.opacity;
-                    let renderer = this.core.getSettings().renderer;
-                    let scaleRatio = Math.pow(2,zoomLevel);
-
-                    const viewportPaddingX = Math.max(0, (renderer._viewport.width - renderer.layout.dimensions.width) / 2);
-                    const viewportPaddingY = Math.max(0, (renderer._viewport.height - renderer.layout.dimensions.height) / 2);
-
-                    // The following absolute values are experimental values to highlight the square on the first page of Salzinnes, CDN-Hsmu M2149.L4
-                    // The relative values are used to scale the highlights according to the zoom level on the page itself
-                    let absoluteRectOriginX = highlighted.relativeRectOriginX * scaleRatio;
-                    let absoluteRectOriginY = highlighted.relativeRectOriginY * scaleRatio;
-                    let absoluteRectWidth = highlighted.relativeRectWidth * scaleRatio;
-                    let absoluteRectHeight = highlighted.relativeRectHeight * scaleRatio;
-
-                    // This indicates the page on top of which the highlights are supposed to be drawn
-                    let highlightPageIndex = highlighted.pageIndex;
-
-                    if (pageIndex === highlightPageIndex)
-                    {
-                        // Calculates where the highlights should be drawn as a function of the whole webpage coordinates
-                        // (to make it look like it is on top of a page in Diva)
-                        let highlightXOffset = renderer._getImageOffset(pageIndex).left - renderer._viewport.left + viewportPaddingX + absoluteRectOriginX;
-                        let highlightYOffset = renderer._getImageOffset(pageIndex).top - renderer._viewport.top + viewportPaddingY + absoluteRectOriginY;
-
-                        //Draw the rectangle
-                        let rgba = null;
-                        switch (layer.layerType)
-                        {
-                            case 0:
-                                rgba = "rgba(51, 102, 255, " + opacity + ")";
-                                break;
-                            case 1:
-                                rgba = "rgba(255, 51, 102, " + opacity + ")";
-                                break;
-                            case 2:
-                                rgba = "rgba(255, 255, 10, " + opacity + ")";
-                                break;
-                            case 3:
-                                rgba = "rgba(10, 255, 10, " + opacity + ")";
-                                break;
-                            case 4:
-                                rgba = "rgba(120, 0, 120, " + opacity + ")";
-                                break;
-                            default:
-                                rgba = "rgba(255, 0, 0, " + opacity + ")";
-                        }
-                        renderer._ctx.fillStyle = rgba;
-                        renderer._ctx.fillRect(highlightXOffset, highlightYOffset,absoluteRectWidth,absoluteRectHeight);
-                    }
+                    this.drawHighlight(layer, highlighted, pageIndex, zoomLevel);
                 }
             );
 
+
+            var paths = layer.paths;
+            paths.forEach((group) =>
+                {
+                    var isDown = false;
+                    group.path.forEach((point) =>
+                    {
+                        this.drawPath(layer, point, pageIndex, zoomLevel, isDown);
+                        isDown = true;
+                    });
+                }
+            );
         });
     }
 
@@ -278,17 +409,61 @@ export class HighlightArea
     }
 }
 
+export class Path
+{
+    constructor ()
+    {
+        this.path = [];
+    }
+
+    addPointToPath(point)
+    {
+        this.path.push(point);
+    }
+}
+
+export class Point
+{
+    constructor (relativeOriginX, relativeOriginY, pageIndex)
+    {
+        this.relativeOriginX = relativeOriginX;
+        this.relativeOriginY = relativeOriginY;
+        this.pageIndex = pageIndex;
+    }
+}
+
 export class Layer
 {
-    constructor (layerType, opacity, highlights)
+    constructor (layerType, opacity, areas)
     {
         this.layerType = layerType;
         this.opacity = opacity;
-        this.highlights = highlights;
+        this.areas = areas;
+        this.paths = [];
     }
 
-    addHighlightToLayer(highlight){
-        this.highlights.push(highlight);
+    addAreaToLayer(area)
+    {
+        this.areas.push(area);
+    }
+
+    addPathToLayer(path)
+    {
+        this.paths.push(path);
+    }
+
+    addToLastPath(point)
+    {
+        // FIXME: Need to check that the list of paths is not empty
+        if (this.paths !== null)
+        {
+            this.paths[this.paths.length - 1].addPointToPath(point);
+        }
+    }
+
+    createNewPath(point)
+    {
+        this.paths.push(new Path());
     }
 }
 
